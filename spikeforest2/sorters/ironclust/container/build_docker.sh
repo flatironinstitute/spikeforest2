df2bb9c5e9c915d53c31e3d220195069eb356fd1
James Jun <jamesjun@gmail.com>
2019-12-12 16:24:13 -0500

    ironclust updated

--- spikeforest2/sorters/ironclust/container/build_docker.sh
+++ spikeforest2/sorters/ironclust/container/build_docker.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-docker build -t magland/sf-ironclust:5.0.5 .
+#docker build -t magland/sf-ironclust:5.0.5 .
+docker build -t jamesjun/sf-ironclust:5.2.15 .
--- spikeforest2/sorters/ironclust/container/push_docker.sh
+++ spikeforest2/sorters/ironclust/container/push_docker.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-docker push magland/sf-ironclust:5.0.5
+#docker push magland/sf-ironclust:5.0.5
+docker push jamesjun/sf-ironclust:5.2.15

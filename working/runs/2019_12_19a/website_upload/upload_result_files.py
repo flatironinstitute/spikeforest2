6609bec6ac1d1f20478f72efa89ec1179e829834
Jeremy Magland <jmagland@flatironinstitute.org>
2019-12-19 09:33:37 -0500

    update analysis run

--- working/runs/2019_12_19a/website_upload/upload_result_files.py
+++ working/runs/2019_12_19a/website_upload/upload_result_files.py
@@ -7,19 +7,15 @@ import kachery as ka
 from mountaintools import client as mt
 
 help_txt = """
-This script saves collections in the following .json files in an output directory
-
-Algorithms.json
-Sorters.json
-SortingResults.json
-StudySets.json
-StudyAnalysisResults.json
+Upload result files to kachery
 """
 
 
 def main():
     parser = argparse.ArgumentParser(description=help_txt, formatter_class=argparse.RawTextHelpFormatter)
     parser.add_argument('path', help='Path to the assembled website data in .json format')
+    parser.add_argument('--console-out', help='Store console output', action='store_true')
+    parser.add_argument('--firings', help='Store firings output', action='store_true')
 
     args = parser.parse_args()
 
@@ -32,10 +28,14 @@ def main():
     for sr in SortingResults:
         print(sr['studyName'], sr['recordingName'])
         console_out_fname = ka.load_file(sr['consoleOut'])
-        mt.createSnapshot(path=console_out_fname, upload_to='spikeforest.public')
-        if sr.get('firings', None) is not None:
-            firings_fname = ka.load_file(sr['firings'])
-            mt.createSnapshot(path=firings_fname, upload_to='spikeforest.public')
+        if args.console_out:
+            mt.createSnapshot(path=console_out_fname, upload_to='spikeforest.public')
+            ka.store_file(console_out_fname, to='default_readwrite')
+        if args.firings:
+            if sr.get('firings', None) is not None:
+                firings_fname = ka.load_file(sr['firings'])
+                mt.createSnapshot(path=firings_fname, upload_to='spikeforest.public')
+                ka.store_file(firings_fname, to='default_readwrite')
 
 if __name__ == "__main__":
     main()

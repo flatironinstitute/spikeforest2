718fc8e95c74bd12730fff81aa9bd2bfb86f5e32
Jeremy Magland <jmagland@flatironinstitute.org>
2019-12-20 16:35:40 -0500

    update test

--- working/tests/test_waveclus_monotrode.py
+++ working/tests/test_waveclus_monotrode.py
@@ -4,11 +4,11 @@ import os
 from spikeforest2_utils import test_sort_monotrode
 
 def main():
-    srcdir = os.path.dirname(os.path.realpath(__file__))
-    os.environ['WAVECLUS_PATH'] = os.path.join(srcdir, '..', '..', 'spikeforest2', 'sorters', 'waveclus', 'matlab', 'wave_clus')
-    container = None
+    # srcdir = os.path.dirname(os.path.realpath(__file__))
+    # os.environ['WAVECLUS_PATH'] = os.path.join(srcdir, '..', '..', 'spikeforest2', 'sorters', 'waveclus', 'matlab', 'wave_clus')
+    # container = None
 
-    # container = 'default'
+    container = 'default'
 
     test_sort_monotrode(sorter_name='waveclus', min_avg_accuracy=0.15, container=container)
 

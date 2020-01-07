a3e27b0ef1287358d82f4e87529c3e7ec863c40d
Jeremy Magland <jmagland@flatironinstitute.org>
2020-01-07 12:22:42 -0500

    add new run

--- spikeforest2_utils/autoextractors/__init__.py
+++ spikeforest2_utils/autoextractors/__init__.py
@@ -1,3 +1,4 @@
 from .autosortingextractor import AutoSortingExtractor
 from .autorecordingextractor import AutoRecordingExtractor
 from .mdaextractors import DiskReadMda, readmda, writemda32, writemda64, writemda, appendmda
+from .mdaextractors import MdaRecordingExtractor, MdaSortingExtractor

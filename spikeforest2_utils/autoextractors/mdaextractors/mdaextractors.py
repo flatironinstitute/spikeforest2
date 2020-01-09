7bf2d2ed40f5382cb83b6f86bac6fb47b0d2bf91
Jeremy Magland <jmagland@flatironinstitute.org>
2020-01-09 10:20:54 -0500

    improve auto recording extractor

--- spikeforest2_utils/autoextractors/mdaextractors/mdaextractors.py
+++ spikeforest2_utils/autoextractors/mdaextractors/mdaextractors.py
@@ -31,7 +31,13 @@ class MdaRecordingExtractor(RecordingExtractor):
         if download:
             path0 = ka.load_file(path=self._timeseries_path)
             if not path0:
-                raise Exception('Unable to realize file: ' + self._timeseries_path)
+                raise Exception('Unable to download file: ' + self._timeseries_path)
+            if geom_path is not None:
+                if not ka.load_file(geom_path):
+                    raise Exception('Unable to download file: ' + geom_path)
+            if params_path is not None:
+                if not ka.load_file(params_path):
+                    raise Exception('Unable to download file: ' + params_path)
             self._timeseries_path = path0
 
         self._timeseries = DiskReadMda(self._timeseries_path)

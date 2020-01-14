6bccb28cb48457211a1de26b57c68489e5cbe661
Jeremy Magland <jmagland@flatironinstitute.org>
2020-01-14 18:26:57 -0500

    developing multisort

--- spikeforest_widgets/widgets/__init__.py
+++ spikeforest_widgets/widgets/__init__.py
@@ -3,3 +3,4 @@ from .Analysis import Analysis
 from .Recording import Recording
 from .TimeseriesView import TimeseriesView
 from .ElectrodeGeometry import ElectrodeGeometry
+from .MultiSort import MultiSort
--- spikeforest_widgets/widgets/index.js
+++ spikeforest_widgets/widgets/index.js
@@ -3,3 +3,4 @@ export { default as Analysis } from './Analysis/Analysis';
 export { default as Recording } from './Recording/Recording';
 export { default as TimeseriesView } from './TimeseriesView/TimeseriesView';
 export { default as ElectrodeGeometry } from './ElectrodeGeometry/ElectrodeGeometry';
+export { default as MultiSort } from './MultiSort/MultiSort';

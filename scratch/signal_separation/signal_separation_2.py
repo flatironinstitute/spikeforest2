d274652de6fe31ee2a918c3e23c89b406c80aaa2
Jeremy Magland <jmagland@flatironinstitute.org>
2020-01-15 22:07:40 -0500

    developing signal separation

--- scratch/signal_separation/signal_separation_2.py
+++ scratch/signal_separation/signal_separation_2.py
@@ -12,7 +12,8 @@ ka.set_config(fr='default_readonly')
 
 # Load a recording
 # recdir = 'sha1dir://fb52d510d2543634e247e0d2d1d4390be9ed9e20.synth_magland/datasets_noise10_K10_C4/001_synth'
-recdir = 'sha1dir://c0879a26f92e4c876cd608ca79192a84d4382868.manual_franklab/tetrode_600s/sorter1_1'
+recdir = 'sha1dir://fb52d510d2543634e247e0d2d1d4390be9ed9e20.synth_magland/datasets_noise20_K20_C4/001_synth'
+# recdir = 'sha1dir://c0879a26f92e4c876cd608ca79192a84d4382868.manual_franklab/tetrode_600s/sorter1_1'
 recobj = dict(
     raw=recdir + '/raw.mda',
     params=ka.load_object(recdir + '/params.json'),
@@ -24,6 +25,8 @@ assert ka.load_file(recobj['raw']) is not None
 def main():
     import spikeextractors as se
     from spikeforest2_utils import writemda32, AutoRecordingExtractor
+    from sklearn.neighbors import NearestNeighbors
+    from sklearn.cross_decomposition import PLSRegression
     import spikeforest_widgets as sw
     sw.init_electron()
 
@@ -38,38 +41,92 @@ def main():
     
     detect_threshold = 3
     detect_interval = 200
+    detect_interval_reference = 10
     detect_sign = -1
     num_events = 1000
     snippet_len = (200, 200)
     window_frac = 0.3
-    channel_index = 0
+    num_passes = 20
+    npca = 100
+    max_t = 30000 * 100
+    k = 20
+    ncomp = 4
     
     R = AutoRecordingExtractor(recobj2)
 
     X = R.get_traces()
-    X_signal = np.zeros((R.get_num_channels(), R.get_num_frames()), dtype=np.float32)
     
-    sig = X[channel_index, :]
+    sig = X.copy()
+    if detect_sign < 0:
+        sig = -sig
+    elif detect_sign == 0:
+        sig = np.abs(sig)
+    sig = np.max(sig, axis=0)
     noise_level = np.median(np.abs(sig)) / 0.6745  # median absolute deviation (MAD)
-    times = detect_on_channel(sig, detect_threshold=noise_level*detect_threshold, detect_interval=detect_interval, detect_sign=detect_sign, margin=1000)
-    times = times[0:num_events]
+    times_reference = detect_on_channel(sig, detect_threshold=noise_level*detect_threshold, detect_interval=detect_interval_reference, detect_sign=1, margin=1000)
+    times_reference = times_reference[times_reference <= max_t]
+    print(f'Num. reference events = {len(times_reference)}')
 
-    snippets = extract_snippets(X, reference_frames=times, snippet_len=snippet_len)
-    tt = np.linspace(-1, 1, snippets.shape[2])
+    snippets_reference = extract_snippets(X, reference_frames=times_reference, snippet_len=snippet_len)
+    tt = np.linspace(-1, 1, snippets_reference.shape[2])
     window0 = np.exp(-tt**2/(2*window_frac**2))
-    for j in range(snippets.shape[0]):
-        for m in range(snippets.shape[1]):
-            snippets[j, m, :] = snippets[j, m, :] * window0
-    A = snippets.reshape(snippets.shape[0], snippets.shape[1] * snippets.shape[2])
+    for j in range(snippets_reference.shape[0]):
+        for m in range(snippets_reference.shape[1]):
+            snippets_reference[j, m, :] = snippets_reference[j, m, :] * window0
+    A_snippets_reference = snippets_reference.reshape(snippets_reference.shape[0], snippets_reference.shape[1] * snippets_reference.shape[2])
 
-    A_denoised = knn_denoise(A, k=20, ncomp=4)
-    snippets_denoised = A_denoised.reshape(snippets.shape)
+    print('PCA...')
+    u, s, vh = np.linalg.svd(A_snippets_reference)
+    components_reference = vh[0:npca, :].T
+    features_reference = A_snippets_reference @ components_reference
 
-    for j in range(len(times)):
-        t0 = times[j]
-        snippet_denoised_0 = np.squeeze(snippets_denoised[j, :, :])
-        X_signal[:, t0-snippet_len[0]:t0+snippet_len[1]] = X_signal[:, t0-snippet_len[0]:t0+snippet_len[1]] + snippet_denoised_0
-        X[:, t0-snippet_len[0]:t0+snippet_len[1]] = X[:, t0-snippet_len[0]:t0+snippet_len[1]] - snippet_denoised_0
+    print('Setting up nearest neighbors...')
+    nbrs = NearestNeighbors(n_neighbors=k + 1, algorithm='ball_tree').fit(features_reference)
+
+    X_signal = np.zeros((R.get_num_channels(), R.get_num_frames()), dtype=np.float32)
+
+    for passnum in range(num_passes):
+        print(f'Pass {passnum}')
+        sig = X.copy()
+        if detect_sign < 0:
+            sig = -sig
+        elif detect_sign == 0:
+            sig = np.abs(sig)
+        sig = np.max(sig, axis=0)
+        noise_level = np.median(np.abs(sig)) / 0.6745  # median absolute deviation (MAD)
+        times = detect_on_channel(sig, detect_threshold=noise_level*detect_threshold, detect_interval=detect_interval, detect_sign=1, margin=1000)
+        times = times[times <= max_t]
+        print(f'Number of events: {len(times)}')
+        if len(times) == 0:
+            break
+        snippets = extract_snippets(X, reference_frames=times, snippet_len=snippet_len)
+        for j in range(snippets.shape[0]):
+            for m in range(snippets.shape[1]):
+                snippets[j, m, :] = snippets[j, m, :] * window0
+        A_snippets = snippets.reshape(snippets.shape[0], snippets.shape[1] * snippets.shape[2])
+        features = A_snippets @ components_reference
+        
+        print('Finding nearest neighbors...')
+        distances, indices = nbrs.kneighbors(features)
+        features2 = np.zeros(features.shape, dtype=features.dtype)
+        print('PLS regression...')
+        for j in range(features.shape[0]):
+            print(f'{j+1} of {features.shape[0]}')
+            inds0 = np.squeeze(indices[j, :])
+            inds0 = inds0[1:] # TODO: it may not always be necessary to exclude the first -- how should we make that decision?
+            f_neighbors = features_reference[inds0, :]
+            pls = PLSRegression(n_components=ncomp)
+            pls.fit(f_neighbors.T, features[j, :].T)
+            features2[j, :] = pls.predict(f_neighbors.T).T
+        A_snippets_denoised = features2 @ components_reference.T
+        
+        snippets_denoised = A_snippets_denoised.reshape(snippets.shape)
+
+        for j in range(len(times)):
+            t0 = times[j]
+            snippet_denoised_0 = np.squeeze(snippets_denoised[j, :, :])
+            X_signal[:, t0-snippet_len[0]:t0+snippet_len[1]] = X_signal[:, t0-snippet_len[0]:t0+snippet_len[1]] + snippet_denoised_0
+            X[:, t0-snippet_len[0]:t0+snippet_len[1]] = X[:, t0-snippet_len[0]:t0+snippet_len[1]] - snippet_denoised_0
 
     S = np.concatenate((X_signal, X, R.get_traces()), axis=0)
 
@@ -121,7 +178,7 @@ def extract_snippets(X, *, reference_frames, snippet_len):
         snippets[i] = snippet_chunk
     return snippets
 
-def knn_denoise(X, *, k, ncomp):
+def knn_denoise(X, X_reference, *, k, ncomp):
     from sklearn.cross_decomposition import PLSRegression
     from sklearn.neighbors import NearestNeighbors
     # Xb = X * window

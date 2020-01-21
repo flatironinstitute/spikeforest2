6a1755550a093c3743174931bc8656f337aafc95
James Jun <jamesjun@gmail.com>
2020-01-21 16:14:58 -0500

    ironclust v5.5.10, merge_thresh=.98 by default

--- spikeforest2/sorters/ironclust/_ironclustsorter.py
+++ spikeforest2/sorters/ironclust/_ironclustsorter.py
@@ -43,7 +43,7 @@ class IronClustSorter(BaseSorter):
         prm_template_name='',  # .prm template file name
         freq_min=300,
         freq_max=8000,
-        merge_thresh=0.99,  # Threshold for automated merging
+        merge_thresh=0.98,  # Threshold for automated merging
         pc_per_chan=0,  # Number of principal components per channel
         whiten=False,  # Whether to do channel whitening as part of preprocessing
         filter_type='bandpass',  # none, bandpass, wiener, fftdiff, ndiff

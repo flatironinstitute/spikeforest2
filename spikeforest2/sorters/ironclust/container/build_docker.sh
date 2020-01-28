036eaa1a31703b46a7184854f1b4f1f03ec54ac2
James Jun <jamesjun@gmail.com>
2020-01-27 21:00:37 -0500

    ironclust v5.6.1

--- spikeforest2/sorters/ironclust/container/build_docker.sh
+++ spikeforest2/sorters/ironclust/container/build_docker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker build -t jamesjun/sf-ironclust:5.5.14 .
+docker build -t jamesjun/sf-ironclust:5.6.1 .
--- spikeforest2/sorters/ironclust/container/push_docker.sh
+++ spikeforest2/sorters/ironclust/container/push_docker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker push jamesjun/sf-ironclust:5.5.14
+docker push jamesjun/sf-ironclust:5.6.1

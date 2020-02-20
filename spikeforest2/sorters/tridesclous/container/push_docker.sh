1b24fd1a31dc336feb3767f5301f0e8afc742bff
Samuel Garcia <sam.garcia.die@gmail.com>
2020-02-20 14:09:18 +0100

    Prepare release of tridesclous 1.5.0

--- spikeforest2/sorters/tridesclous/container/push_docker.sh
+++ spikeforest2/sorters/tridesclous/container/push_docker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker push magland/sf-tridesclous:1.4.3
-#docker push samuelgarcialyon/sf-tridesclous:1.4.2
+# docker push magland/sf-tridesclous:1.4.3
+docker push samuelgarcialyon/sf-tridesclous:1.5.0
